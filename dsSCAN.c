a022112098ae6759be8ff76358168515a4872fee
s3r71g <96942361+s3r71g@users.noreply.github.com>
2023-08-30 23:54:03 +0530

    Create dsSCAN.c

---
+++ dsSCAN.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int RQ[100],i,j,n,TotalHeadMoment=0,initial,size,move;
+    printf("Enter the number of Requests\n");
+    scanf("%d",&n);
+    printf("Enter the Requests sequence\n");
+    for(i=0;i<n;i++)
+     scanf("%d",&RQ[i]);
+    printf("Enter initial head position\n");
+    scanf("%d",&initial);
+    printf("Enter total disk size\n");
+    scanf("%d",&size);
+    printf("Enter the head movement direction for high 1 and for low 0\n");
+    scanf("%d",&move);
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+
+      {
+        if(RQ[j]>RQ[j+1])
+        {
+            int temp;
+            temp=RQ[j];
+            RQ[j]=RQ[j+1];
+            RQ[j+1]=temp;
+        }
+      }
+    }
+    if(move==1)
+    {
+        TotalHeadMoment += size-1 - initial;
+        TotalHeadMoment += size-1 - RQ[0];
+    }
+    else
+    {
+        TotalHeadMoment += initial - 0;
+        TotalHeadMoment += RQ[n-1] - 0;
+    }
+    printf("Total head movement is %d",TotalHeadMoment);
+    return 0;
+}
